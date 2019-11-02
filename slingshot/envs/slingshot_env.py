829e55a1d0f96f59aaeac0a7b7f1f26de48c6a1d
luca-weishaupt <luca.weishaupt@mail.mcgill.ca>
2019-11-02 17:40:01 -0400

    fixed formatting and changed fn names

--- slingshot/envs/slingshot_env.py
+++ slingshot/envs/slingshot_env.py
@@ -5,10 +5,7 @@ from gym.utils import seeding
 ####################
 import random
 import math
-
-
-
-#################
+####################
 FRAME_WIDTH = 20.0
 FRAME_HEIGHT = 10.0
 ROCKET_SPAWN_BOUNDARY_X = 0.1
@@ -23,18 +20,19 @@ PLANET_MAX_MASS = 10**25
 PLANET_MIN_MASS = 10**23
 
 MAX_STEPS = 200
-MAX_INIT_SPEED = 1.0/ (2 * MAX_STEPS)
+MAX_INIT_SPEED = 1.0/(2 * MAX_STEPS)
 MAX_INIT_ANGLE = math.pi / 6
-SPEED_FACTOR = 20 # Km/s
-ROCKET_ACC = 0.02 # km/s^2
+SPEED_FACTOR = 20  # Km/s
+ROCKET_ACC = 0.02  # km/s^2
 ROCKET_ROT_ANGLE = math.pi / 6
 
 FUEL_COST = -1
 TURN_COST = -0.1
 
-STEP_DURATION = 3600 # Seconds
-GRAVITY_CONSTANT = 6.674 * 10 **-20 # km^3 / (kg s^2)
-DISTANCE_FACTOR =  SPEED_FACTOR / MAX_INIT_SPEED * STEP_DURATION
+STEP_DURATION = 3600  # Seconds
+GRAVITY_CONSTANT = 6.674 * 10 ** (-20)  # km^3 / (kg s^2)
+DISTANCE_FACTOR = SPEED_FACTOR / MAX_INIT_SPEED * STEP_DURATION
+
 
 class SlingshotEnv(gym.Env):
   metadata = {'render.modes': ['human']}
@@ -47,7 +45,7 @@ class SlingshotEnv(gym.Env):
       """
 
       self.target_x = random.uniform(TARGET_SPAWN_BOUNDARY_X, 1)
-      self.target_y = random.uniform(0,1)
+      self.target_y = random.uniform(0, 1)
       self.planet_x = random.uniform(PLANET_SPAWN_BOUNDARY_X_LEFT, PLANET_SPAWN_BOUNDARY_X_RIGHT)
       self.planet_y = random.uniform(PLANET_SPAWN_BOUNDARY_Y_BOT, PLANET_SPAWN_BOUNDARY_Y_TOP)
       self.planet_m = random.uniform(PLANET_MIN_MASS, PLANET_MAX_MASS)
@@ -59,9 +57,6 @@ class SlingshotEnv(gym.Env):
       self.rocket_vel_y = speed * math.sin(self.rocket_angle)
       self.min_distance = math.sqrt((self.target_x - self.rocket_x)**2 + (self.target_y - self.target_y)**2) 
       self.time_step = 0
-
-
-
 
   def step(self, action):
     """
@@ -88,23 +83,20 @@ class SlingshotEnv(gym.Env):
         debugging data (gravity and stuff)
 
     """
-    self.updatePosition(action)
-    done = self.isDone()
+    self.update_position(action)
+    done = self.is_done()
     reward = self.calculate_reward(action, done)
     distance = math.sqrt((self.target_x - self.rocket_x)**2 + (self.target_y - self.target_y)**2) 
     if distance < self.min_distance:
       self.min_distance = distance
     
-    return (self.getObs(), reward, done, dict())
+    return self.get_obs(), reward, done, dict()
 
-
-
-
-  def updatePosition(self, action):
+  def update_position(self, action):
     delta_x = (self.planet_x - self.rocket_x)
     delta_y = (self.planet_y - self.rocket_y)
     r_p = math.sqrt(delta_x**2 + delta_y**2) * DISTANCE_FACTOR
-    a_of_g = 0 #GRAVITY_CONSTANT * self.planet_m / (r_p**2) # km / s^2
+    a_of_g = 0  # GRAVITY_CONSTANT * self.planet_m / (r_p**2) # km / s^2
     a_of_g_x = a_of_g * delta_x / r_p
     a_of_g_y = a_of_g * delta_y / r_p
     a_of_t = ROCKET_ACC * action[0]
@@ -116,19 +108,20 @@ class SlingshotEnv(gym.Env):
     a_x = (a_of_g_x + a_of_t_x)
     a_y = (a_of_g_y + a_of_t_y)
 
-
-    self.rocket_x = (self.rocket_x * DISTANCE_FACTOR + self.rocket_vel_x * SPEED_FACTOR * STEP_DURATION + 1/2 * a_x * STEP_DURATION ** 2) / DISTANCE_FACTOR
-    self.rocket_y = (self.rocket_y * DISTANCE_FACTOR + self.rocket_vel_y * SPEED_FACTOR * STEP_DURATION + 1/2 * a_y * STEP_DURATION ** 2) / DISTANCE_FACTOR
+    self.rocket_x = (self.rocket_x * DISTANCE_FACTOR + self.rocket_vel_x * SPEED_FACTOR * STEP_DURATION + 1/2 * a_x *
+                     STEP_DURATION ** 2) / DISTANCE_FACTOR
+    self.rocket_y = (self.rocket_y * DISTANCE_FACTOR + self.rocket_vel_y * SPEED_FACTOR * STEP_DURATION + 1/2 * a_y *
+                     STEP_DURATION ** 2) / DISTANCE_FACTOR
     self.rocket_vel_x = self.rocket_vel_x + a_x * STEP_DURATION / SPEED_FACTOR
     self.rocket_vel_y = self.rocket_vel_y + a_y * STEP_DURATION / SPEED_FACTOR
     self.rocket_angle = final_angle
 
-  def getObs(self):
+  def get_obs(self):
     return (self.rocket_x, self.rocket_y, self.rocket_vel_x, 
-    self.rocket_vel_y, self.planet_x, self.planet_y, self.planet_m,
-    self.target_x, self.target_y)
+            self.rocket_vel_y, self.planet_x, self.planet_y, self.planet_m,
+            self.target_x, self.target_y)
 
-  def isDone(self):
+  def is_done(self):
     if self.rocket_x > 1 or self.rocket_x < 0:
       return True
     elif self.rocket_y > 1 or self.rocket_y < 0:
@@ -145,14 +138,9 @@ class SlingshotEnv(gym.Env):
       reward += 200/(1+100*self.min_distance) - 2
     return reward
 
-
-
-
-
   def reset(self):
     self.__init__()
-    return self.getObs()
-
+    return self.get_obs()
 
   def render(self, mode='human'):
     pass
